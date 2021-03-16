3f264e2ca1fff585d384d1418c1925958cfc2d5c
Rupesh Garhwal <rupeshgarhwal3920@gmail.com>
2021-03-16 21:42:02 +0530

    add cors

--- server.js
+++ server.js
@@ -1,31 +1,37 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-const connectDB = require('./config/db');
+const cors = require('cors');
 
-const path = require('path');
+const connectDB = require("./config/db");
 
-
+const path = require("path");
 
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 app.use(express.json());
 
 connectDB();
 
+// CORS
+const corsOptions = {
+    origin: process.env.ALLOWED_CLIENTS.split(',')
+}
+app.use(cors(corsOptions));
+
 // Template engine
-app.set('views', path.join(__dirname, '/views'));
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "/views"));
+app.set("view engine", "ejs");
 
 // Routes
-app.use('/api/files', require('./routes/files'));
+app.use("/api/files", require("./routes/files"));
 
-app.use('/files', require('./routes/show'));
+app.use("/files", require("./routes/show"));
 
-app.use('/files/download', require('./routes/download'));
+app.use("/files/download", require("./routes/download"));
 
-app.listen(PORT, ()=> {
-    console.log('Listening on port: ', PORT);
+app.listen(PORT, () => {
+  console.log("Listening on port: ", PORT);
 });
